0c158bd936709616f1a00ed7d031874b9de7b6ab
Abhinav-Kotta <abhinav.kotta@gmail.com>
2024-02-16 16:50:01 -0500

    streamlit sidebar

--- conversation.py
+++ conversation.py
@@ -1,7 +1,4 @@
 import os
-import sys
-import constants
-import openai
 from langchain_community.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_openai import ChatOpenAI
@@ -44,20 +41,21 @@ def text_to_speech(text_response):
 
     response.stream_to_file(speech_file_path)
 
-while True:
-    query = record_and_transcribe_audio()
-    if query.lower() == 'bye':
-        break
+def conversation_AI():
+    while True:
+        query = record_and_transcribe_audio()
+        if query.lower() == 'bye':
+            break
 
-    with open(filename, 'a') as f:
-        f.write('\n')  # Write a newline character
-        f.write(f'customer: {query}\n')  # Write your text
-    loader1 = TextLoader("data.txt")
-    loader2 = TextLoader(filename)
-    index = VectorstoreIndexCreator().from_loaders([loader1, loader2])
+        with open(filename, 'a') as f:
+            f.write('\n')  # Write a newline character
+            f.write(f'customer: {query}\n')  # Write your text
+        loader1 = TextLoader("data.txt")
+        loader2 = TextLoader(filename)
+        index = VectorstoreIndexCreator().from_loaders([loader1, loader2])
 
-    response = index.query(query, llm=ChatOpenAI())
-    text_to_speech(response)
-    with open(filename, 'a') as f:
-        f.write(f'AI: {response}\n')
-    print(f'AI: {response}')
+        response = index.query(query, llm=ChatOpenAI())
+        text_to_speech(response)
+        with open(filename, 'a') as f:
+            f.write(f'AI: {response}\n')
+        print(f'AI: {response}')
