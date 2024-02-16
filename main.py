8268e54ba0f7dd6beb19fa94d8c8aedfa7a7208a
Abhinav-Kotta <abhinav.kotta@gmail.com>
2024-02-16 15:19:11 -0500

    made it better

--- main.py
+++ main.py
@@ -5,13 +5,31 @@ import openai
 from langchain_community.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_openai import ChatOpenAI
+from datetime import datetime
+import uuid
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-query = sys.argv[1]
-print(query)
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+uid = uuid.uuid4()
+filename = f'{timestamp}_{uid}.txt'
 
-loader = TextLoader("data.txt")
-index = VectorstoreIndexCreator().from_loaders([loader])
 
-print(index.query(query, llm=ChatOpenAI()))
+while True:
+    query = input("You: ")
+    if query.lower() == 'bye':
+        break
+
+    with open(filename, 'a') as f:
+        f.write('\n')  # Write a newline character
+        f.write(f'customer: {query}\n')  # Write your text
+    loader1 = TextLoader("data.txt")
+    loader2 = TextLoader(filename)
+    index = VectorstoreIndexCreator().from_loaders([loader1, loader2])
+
+    response = index.query(query, llm=ChatOpenAI())
+
+    with open(filename, 'a') as f:
+        f.write(f'AI: {response}\n')
+    print(f'AI: {response}')
