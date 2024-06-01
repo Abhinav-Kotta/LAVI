a418d49ca1bdc3a5db20f2df077d5ab2b92fcdec
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2024-06-01 19:31:06 -0400

    "added crud operation apis"

--- pymongo-test/main.py
+++ pymongo-test/main.py
@@ -23,7 +23,7 @@ def add_customer(phone, name, appointment_datetimes):
     customer = {
         "phone": phone,
         "name": name,
-        "appointment": appointment_datetimes
+        "appointment_dates": [appointment_datetimes]
     }
     collection.insert_one(customer)
 
@@ -39,9 +39,29 @@ def get_single_customer_by_phone(phone):
     customer = collection.find_one({"phone": phone})
     return customer
 
-
 def reschedule_customer_appt(old_appt_date, new_appt_date, phone):
+    if not check_appointment_available(new_appt_date):
+        return False
+    customer = get_single_customer_by_phone(phone)
+    customer['appointment_dates'].remove(old_appt_date)
+    customer['appointment_dates'].append(new_appt_date)
     collection.update_one(
-        {"phone": phone}, {"$pull": {"appointments": old_appt_date}})
+        {"name": customer['name']}, {"$set": {"appointment_dates": customer['appointment_dates']}})
+
+# pull phone number to get customer 
+def schedule_appointment(phone, date):
+    if not check_appointment_available(date):
+        return False
+    customer = get_single_customer_by_phone(phone)
+    customer['appointment_dates'].append(date)
     collection.update_one(
-        {"phone": phone}, {"$push": {"appointments": new_appt_date}})
+        {"name": customer['name']}, {"$set": {"appointment_dates": customer['appointment_dates']}})
+    
+def check_appointment_available(date):
+    customers = get_all_customers()
+    for customer in customers:
+        if "appointment_dates" in customer.keys() and date in customer['appointment_dates']:
+            return False
+    return True
+
+
